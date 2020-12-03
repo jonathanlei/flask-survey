9c175ce4802f5cb823b170f87e398f9a43516340
Claire Casey <clcasey805@gmail.com>
2020-12-03 15:40:17 -0800

    post code review through part 8

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
-
 @app.route("/")
 def generate_homepage():
     """ generate a homepage with title, instruction and start button of a survey"""
@@ -23,13 +20,20 @@ def generate_homepage():
 @app.route("/begin", methods=["POST"])
 def start_survey():
     """ redirecting to question page, and reset responses list"""
-    responses.clear()
+    session["responses"] = []
     return redirect("/questions/0")
 
 
 @app.route("/questions/<int:number>")
 def generate_question(number):
-    """ generate a question page from the question list based on the number"""
+    """ generate a question page from the question list based on the number """
+    # check if the number from the url is the correct question the user should 
+    # be answering
+    current_response_num = len(session["responses"])
+    if number != current_response_num:
+        flash(f"Invalid number {number} in survey")
+        return redirect(f"/questions/{current_response_num}")
+
     question = survey.questions[number]
     return render_template(
         "question.html",
@@ -40,7 +44,12 @@ def generate_question(number):
 @app.route("/answer/<int:number>", methods=["POST"])
 def store_answer(number):
     """ store the answer and redirect to next question page"""
+    responses = session["responses"]
     responses.append(request.form["answer"])
+    session["responses"] = responses
+    
+    print(session["responses"], "session responses")
+
     redirect_url = (
         f"/questions/{number+1}"
         if len(responses) < len(survey.questions)
@@ -51,5 +60,4 @@ def store_answer(number):
 @app.route("/thanks")
 def generate_thanks():
     """ show the thank you page"""
-    print(responses, survey.questions)
     return render_template("completion.html")
